481216087671e21b298c1bbda0e0d7b8431e56ea
titanceo <titan8ceo@hotmail.com>
2020-07-11 20:46:59 -0500

    creacion de menu, submenu y content

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 import {compileTemplate} from './lib/compileTemplate.js';
-import {menu} from './lib/data.js';
+import {menu,subMenus} from './lib/data.js';
 
 const ct = new compileTemplate();
 
 ct.actionCompile('template_menu',menu,'menu')
+
+window.showMenu = function(valor){
+  ct.actionCompile('template_submenu',subMenus[valor],'submenu');
+}
+
+window.showContent = function(file){
+  
+  $.ajax({
+    url: file,
+    dataType: "html"
+  }).done(function(html){
+    document.getElementById('content').innerHTML=html;
+  });
+}
+
